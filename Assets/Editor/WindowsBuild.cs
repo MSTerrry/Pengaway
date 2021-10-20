b1aea23be00ddc3bd12e697ee8bfc4b6410cc0fe
MSTerrrry <mishatuhba@gmail.com>
2021-10-20 22:06:25 +0300

    Fix buildScript

--- Assets/Editor/WindowsBuild.cs
+++ Assets/Editor/WindowsBuild.cs
@@ -9,6 +9,21 @@ public class WindowsBuild : MonoBehaviour
     [MenuItem("Build/BuildWindows")]
     public static void BuildWindows()
     {
-        TestBuild.Build();
+        var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Windows";
+        var outputPath = Path.Combine(outdir, Application.productName + ".exe");
+
+        if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+        if (File.Exists(outputPath)) File.Delete(outputPath);
+
+        string[] scenes = new[] { "Assets/Scenes/Menu.unity", "Assets/Scenes/Game.unity" };
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        if (File.Exists(outputPath))
+        {
+            Debug.Log("Build Success :" + outputPath);
+        }
+        else
+        {
+            Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        };
     }
 }
